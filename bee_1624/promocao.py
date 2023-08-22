644e4497ef7c21f1d4c77d5c2994008a5a6bb53c
Edy010Reys <edson10vasconcelos@gmail.com>
2023-08-22 17:25:16 -0300

    beecrowd 1624

--- bee_1624/promocao.py
+++ bee_1624/promocao.py
@@ -2,20 +2,13 @@ while True:
     cases = int(input())
     if cases == 0:
         break
-
     products = []
-
-    for _ in range(cases):
-        price, weight = map(int, input().split())
+    for var in range(cases):
+        price, weight = map(int, input().split(' '))
         products.append((price, weight))
-
     max_weight = int(input())
-
     dp = [0] * (max_weight + 1)
-
     for price, weight in products:
         for w in range(max_weight, weight - 1, -1):
             dp[w] = max(dp[w], dp[w - weight] + price)
-
     print(dp[max_weight])
-   
