83b10335ef212fe1a634c1c0463a1da9daa5d6ce
Edson <edson@desktop-Ubuntu22.04>
2024-01-19 17:13:26 -0300

    beecrowd 1624

--- bee_1624/promocao.py
+++ bee_1624/promocao.py
@@ -1,14 +1,40 @@
 while True:
-    cases = int(input())
-    if cases == 0:
+    list_products = [
+        tuple(map(int, input().split(' '))) 
+        for _ in range(int(input()))
+    ]
+
+    if not list_products:
         break
-    products = []
-    for var in range(cases):
-        price, weight = map(int, input().split(' '))
-        products.append((price, weight))
-    max_weight = int(input())
-    dp = [0] * (max_weight + 1)
-    for price, weight in products:
-        for w in range(max_weight, weight - 1, -1):
-            dp[w] = max(dp[w], dp[w - weight] + price)
-    print(dp[max_weight])
+
+    list_products.sort(
+        key= lambda data: data[0] / data[1],
+        reverse=True
+    )
+
+    weight_max = int(input())
+    price_bigger = 0  
+    for index in range(len(list_products)):
+        for product in list_products:
+            list_loop = [
+                product_list
+                for product_list in list_products
+                if product_list != product
+            ]
+
+            list_loop.insert(index, product)
+
+            price_total = 0
+            weight_total = 0
+            for price, weight in list_loop:          
+                weight_total += weight
+
+                if weight_total > weight_max:
+                    price_bigger = max(
+                        price_bigger, price_total
+                    )
+                    break
+                
+                price_total += price
+    print(price_bigger)
+    
